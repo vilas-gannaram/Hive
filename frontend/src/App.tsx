ce7f9b29493dbc367d1b4817d48c9d18b002171c
vilas <vilasgannaram@gmail.com>
2024-08-26 21:06:22 +0530

    added UI boilerplate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { lazy } from 'react';
+
+const Home = lazy(() => import('@/pages/home'));
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<main className="mt-[1rem] mx-auto px-[5vw] max-w-screen-xl">
+			<Home />
+		</main>
+	);
 }
 
 export default App;
